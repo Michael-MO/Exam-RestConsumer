58fbc622fd52107f5bddf2826966b5d99889a7cc
Michael-MO <Michael-MO@users.noreply.github.com>
2019-01-07 19:55:40 +0100

    Init Commit

---
+++ RestConsumer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RestConsumer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            RestConsumer consumer = new RestConsumer();
+            consumer.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RestConsumer/RestConsumer.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace RestConsumer
+{
+    public class RestConsumer
+    {
+        private const string URI = "https://mmo-restservicetest4.azurewebsites.net/api/meassurements";
+
+        public RestConsumer() { }
+
+        public void Start()
+        {
+            var restDatas = GetAll();
+
+            foreach (Meassurement data in restDatas)
+            {
+                Console.WriteLine(data);
+            }
+        }
+
+        private IList<Meassurement> GetAll()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string content = client.GetStringAsync(URI).Result;
+                IList<Meassurement> cList = JsonConvert.DeserializeObject<IList<Meassurement>>(content);
+                return cList;
+            }
+        }
+
+        private Meassurement GetOne(int id)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string content = client.GetStringAsync(URI).Result;
+                Meassurement obj = JsonConvert.DeserializeObject<Meassurement>(content);
+                return obj;
+            }
+        }
+    }
+}
